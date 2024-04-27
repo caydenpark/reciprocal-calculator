ccd1521cd6282b3e3a324c8323596ec1c3e5eb78
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-04-27 15:42:07 -0600

    calculator is now functional

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 // }
 // let user = "Jane User";
 // document.body.textContent = greeter(user);
+function calculate() {
+    var percentageChangeInput = document.getElementById('a');
+    var percentageChange = parseFloat(percentageChangeInput.value);
+    var percentageChangeReciprocal = ((-percentageChange) / (1 + percentageChange));
+    var reciprocalInput = document.getElementById('b');
+    reciprocalInput.value = percentageChangeReciprocal.toFixed(2).toString();
+}
+var elementA = document.getElementById('a');
+if (elementA) {
+    elementA.addEventListener('input', calculate);
+}
--- index.ts
+++ index.ts
@@ -6,3 +6,17 @@
    
 // document.body.textContent = greeter(user);
 
+
+function calculate() {
+    let percentageChangeInput = document.getElementById('a') as HTMLInputElement;
+    let percentageChange: number = parseFloat(percentageChangeInput.value);
+    let percentageChangeReciprocal = ((-percentageChange) / (1 + percentageChange));
+
+    let reciprocalInput = document.getElementById('b') as HTMLInputElement;
+    reciprocalInput.value = percentageChangeReciprocal.toFixed(2).toString();
+}
+
+const elementA = document.getElementById('a');
+if (elementA) {
+    elementA.addEventListener('input', calculate);
+}
