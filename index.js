27434f52c37d642dc6dd0eea9ec581f7c6c31f51
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-04-28 21:37:15 -0600

    Automatically adds percent sign to both values

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function calculate() {
     var percentageChangeInput = document.getElementById('a');
     var percentageChange = parseFloat(percentageChangeInput.value);
-    var percentageChangeReciprocal = ((-percentageChange) / (1 + percentageChange));
+    var percentageChangeReciprocal = ((-(percentageChange / 100)) / (1 + (percentageChange / 100))) * 100;
     var reciprocalInput = document.getElementById('b');
-    reciprocalInput.value = percentageChangeReciprocal.toFixed(2).toString();
+    if (percentageChangeInput.value !== '' && percentageChangeInput.value !== '-') {
+        reciprocalInput.value = percentageChangeReciprocal.toFixed(0).toString() + '%';
+    }
+    else {
+        reciprocalInput.value = '';
+    }
+}
+function addPercentSign(event) {
+    // Check if the key pressed is the "delete" button (keycode 46)
+    if (event.key !== 'Delete' && event.key !== 'Backspace') {
+        var elementA_1 = document.getElementById('a');
+        var elementAValue = elementA_1.value.replace(/%/g, '');
+        elementA_1.value = elementAValue + '%';
+    }
 }
 var elementA = document.getElementById('a');
 if (elementA) {
+    elementA.addEventListener('keyup', addPercentSign);
     elementA.addEventListener('input', calculate);
 }
--- index.ts
+++ index.ts
@@ -1,13 +1,25 @@
 function calculate() {
     let percentageChangeInput = document.getElementById('a') as HTMLInputElement;
     let percentageChange: number = parseFloat(percentageChangeInput.value);
-    let percentageChangeReciprocal = ((-percentageChange) / (1 + percentageChange));
+    let percentageChangeReciprocal = ((-(percentageChange / 100)) / (1 + (percentageChange / 100))) * 100;
 
     let reciprocalInput = document.getElementById('b') as HTMLInputElement;
-    reciprocalInput.value = percentageChangeReciprocal.toFixed(2).toString();
+    if (percentageChangeInput.value !== '' && percentageChangeInput.value !== '-') {
+        reciprocalInput.value = percentageChangeReciprocal.toFixed(0).toString() + '%';
+    } else { reciprocalInput.value = ''; }
+}
+
+function addPercentSign(event: KeyboardEvent) {
+    // Check if the key pressed is the "delete" button (keycode 46)
+    if (event.key !== 'Delete' && event.key !== 'Backspace') {
+        let elementA = document.getElementById('a') as HTMLInputElement;
+        let elementAValue = elementA.value.replace(/%/g, '');
+        elementA.value = elementAValue + '%';
+    }
 }
 
 const elementA = document.getElementById('a');
 if (elementA) {
+    elementA.addEventListener('keyup', addPercentSign);
     elementA.addEventListener('input', calculate);
 }
