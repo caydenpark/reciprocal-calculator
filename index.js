f497007db0c1840bf21a83b548cb2aef81a5bf18
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-04-29 12:35:36 -0600

    fixed issue with typing numbers in field 1

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ function calculate() {
     var percentageChangeInput = document.getElementById('a');
     var percentageChange = parseFloat(percentageChangeInput.value);
     var percentageChangeReciprocal = ((-(percentageChange / 100)) / (1 + (percentageChange / 100))) * 100;
-    var reciprocalInput = document.getElementById('b');
-    if (percentageChangeInput.value !== '' && percentageChangeInput.value !== '-') {
-        reciprocalInput.value = percentageChangeReciprocal.toFixed(0).toString() + '%';
+    var percentageChangeOutput = document.getElementById('b');
+    if (!['', '-', '%', '-%'].includes(percentageChangeInput.value)) {
+        if (percentageChangeReciprocal > 0) {
+            percentageChangeOutput.value = '+' + percentageChangeReciprocal.toFixed(0).toString() + '%';
+        }
+        else {
+            percentageChangeOutput.value = percentageChangeReciprocal.toFixed(0).toString() + '%';
+        }
     }
     else {
-        reciprocalInput.value = '';
+        percentageChangeOutput.value = '';
     }
 }
 function addPercentSign(event) {
     // Check if the key pressed is the "delete" button (keycode 46)
-    if (event.key !== 'Delete' && event.key !== 'Backspace') {
+    if (!['Delete', 'Backspace', 'ArrowLeft', 'ArrowRight', '-'].includes(event.key)) {
         var elementA_1 = document.getElementById('a');
         var elementAValue = elementA_1.value.replace(/%/g, '');
         elementA_1.value = elementAValue + '%';
+        // Move the cursor to appear before the percent sign
+        elementA_1.setSelectionRange(elementA_1.value.length - 1, elementA_1.value.length - 1);
     }
 }
 var elementA = document.getElementById('a');
--- index.ts
+++ index.ts
@@ -3,18 +3,23 @@ function calculate() {
     let percentageChange: number = parseFloat(percentageChangeInput.value);
     let percentageChangeReciprocal = ((-(percentageChange / 100)) / (1 + (percentageChange / 100))) * 100;
 
-    let reciprocalInput = document.getElementById('b') as HTMLInputElement;
-    if (percentageChangeInput.value !== '' && percentageChangeInput.value !== '-') {
-        reciprocalInput.value = percentageChangeReciprocal.toFixed(0).toString() + '%';
-    } else { reciprocalInput.value = ''; }
+    let percentageChangeOutput = document.getElementById('b') as HTMLInputElement;
+    if (!['', '-', '%', '-%'].includes(percentageChangeInput.value)) {
+        if (percentageChangeReciprocal > 0) {
+            percentageChangeOutput.value = '+' + percentageChangeReciprocal.toFixed(0).toString() + '%';
+        } else {percentageChangeOutput.value = percentageChangeReciprocal.toFixed(0).toString() + '%';}
+    } else { percentageChangeOutput.value = ''; }
 }
 
 function addPercentSign(event: KeyboardEvent) {
     // Check if the key pressed is the "delete" button (keycode 46)
-    if (event.key !== 'Delete' && event.key !== 'Backspace') {
+    if (!['Delete', 'Backspace', 'ArrowLeft', 'ArrowRight', '-'].includes(event.key)) {
         let elementA = document.getElementById('a') as HTMLInputElement;
         let elementAValue = elementA.value.replace(/%/g, '');
         elementA.value = elementAValue + '%';
+
+        // Move the cursor to appear before the percent sign
+        elementA.setSelectionRange(elementA.value.length - 1, elementA.value.length - 1);
     }
 }
 
